47b8d29f4c44a45d0cf6e1dc5ffe48919bb6ba36
kovbasyuk <kovbasyuk_i@tdmu.edu.ua>
2016-05-06 17:26:36 +0300

    New

---
+++ Login_page.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+        <head>
+    <title>Login page</title>
+
+    <link href="/style.css" type=text/css rel=stylesheet>
+    </head>
+<body>
+<form method="post" action="Authorized.php" >
+    <fieldset>
+    <legend><h2> Login to Contact Manager</h2></legend>
+
+     <p><input type="text" name="login" value="" placeholder="Username or Email"></p>
+     <p><input type="text" name="password" value="" placeholder="Password"></p>
+    
+          <h4> <input name="галочка" type="checkbox"> Remember me </h4>
+
+        <p><input type="submit" value="Login">
+    
+      <input type="reset" name="Reset" value="Clear form"></p>
+  
+     <a class="btn" href="Registration%20page.php"> Registration </a>
+
+    </fieldset>
+</form>
+</body>
--- Registration page.php
+++ Registration page.php
@@ -1,9 +1,27 @@
 <!DOCTYPE html>
 <head>
     <title>Registration page</title>
+    <link href="/style.css" type=text/css rel=stylesheet>
 </head>
 
 <body>
-<h2> <input type="text"></h2>
-<h2> <input type="text"></h2>
+<form id='register' action='register.php' method='post'
+      accept-charset='UTF-8'>
+    <fieldset >
+        <legend>Register</legend>
+        <p><label for='name' >Your Full Name: </label>
+        <input type='text' name='name' maxlength="50" /></p>
+        <p><label for='email' >Email Address:</label>
+        <input type='text' name='email' maxlength="50" /></p>
+
+        <p><label for="username" >UserName:</label>
+        <input type='text' name='username' id='username' maxlength="50" /></p>
+
+        <p><label for='password' >Password:</label>
+        <input type='password' name='password'  maxlength="50" /></p>
+        <input type='submit' name='Submit' value='Submit' />
+        <input type="reset" name="Reset" value="Очистить форму">
+
+    </fieldset>
+</form>
 </body>
